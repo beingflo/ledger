bfab3aac87355b28b6286c1e33d69127e06460cf
Florian Marending <florian@marending.dev>
2024-07-22 22:57:38 +0200

    Build categorization logic

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,10 @@
 import { createSignal, type Component } from 'solid-js';
 import { useStore } from '../store';
+import { filterTransactions } from '../utils';
+import { Categorization } from '../types';
 
 const Settings: Component = () => {
-  const [state, { modifyCategorizations }] = useStore();
+  const [state, { modifyCategorizations, updateTransaction }] = useStore();
   const [categorizationJson, setCategorizationJson] = createSignal('');
 
   const onEditEnd = event => {
@@ -11,16 +13,32 @@ const Settings: Component = () => {
     modifyCategorizations(JSON.parse(categorizationJson() ?? ''));
   };
 
+  const applyCategorizations = event => {
+    event?.preventDefault();
+    const categorizations: Array<Categorization> = JSON.parse(categorizationJson());
+
+    categorizations.forEach(cat => {
+      const filteredTransactions = filterTransactions(cat.query, [...state.transactions]);
+      filteredTransactions.forEach(trx => {
+        updateTransaction(trx.id, cat.category, cat.factor);
+      });
+      console.log(filteredTransactions.length, 'transactions updated');
+    });
+  };
+
   return (
     <>
-      <div class="w-full flex flex-col p-4 gap-8 items-start">
+      <div class="w-full flex flex-col p-4 gap-8 items-end">
         <textarea
-          class="w-full h-96 border border-dashed border-gray-400 focus:outline-none"
+          class="p-2 w-full h-96 border border-black focus:outline-none"
           placeholder="content"
-          value={JSON.stringify(state.categorizations ?? [])}
+          value={JSON.stringify(state.categorizations ?? [], null, 2)}
           onInput={event => setCategorizationJson(event?.currentTarget.value)}
         />
-        <button onClick={onEditEnd}>save</button>
+        <div class="flex flex-row gap-4">
+          <button onClick={onEditEnd}>save</button>
+          <button onClick={applyCategorizations}>run</button>
+        </div>
       </div>
     </>
   );
