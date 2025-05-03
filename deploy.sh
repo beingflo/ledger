341d07098b813859cad6d6d65f76f60ad7857475
Florian Marending <florian@marending.dev>
2025-05-03 23:43:25 +0200

    [infra] Remove github ci, add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+die() { echo "$*" 1>&2 ; exit 1; }
+
+echo -e "Deploying jour.rest.quest to production!"
+
+[ -z "$(git status --porcelain)" ] || die "There are uncommitted changes"
+
+npm run build || die "Build failed"
+
+docker --context arm cp dist caddy:/srv/ledger || die "Failed to copy files to container"
